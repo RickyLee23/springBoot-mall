873e4c7366b89c335ce5d275cfedbab5b9f30edd
Ricky Lee <Ricky.Lee4@ibm.com>
2022-08-08 22:03:26 +0800

    建立 Controller-Service-Dao 架構

---
+++ src/main/java/com/example/spring/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.example.spring.dao;
+
+import com.example.spring.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+
+}
---
+++ src/main/java/com/example/spring/rowmapper/ProductRowMapper.java
@@ -0,0 +1,32 @@
+package com.example.spring.rowmapper;
+
+import com.example.spring.model.Product;
+import constant.ProductCategory;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductRowMapper implements RowMapper<Product> {
+
+    @Override
+    public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+        Product product = new Product();
+
+        product.setProductId(resultSet.getInt("product_id"));
+        product.setProductName(resultSet.getString("product_name"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+        product.setImage_url(resultSet.getString("image_url"));
+        product.setPrice(resultSet.getInt("price"));
+        product.setStock(resultSet.getInt("stock"));
+        product.setDescription(resultSet.getString("description"));
+        product.setCreatedDate(resultSet.getTimestamp("created_date"));
+        product.setLastModifiedDate(resultSet.getTime("last_modified_date"));
+
+        return product;
+    }
+}
